8d5fd03849657b293d48ff46f272c0d4af043145
JeffRisberg <j.s.risberg@gmail.com>
2021-04-01 21:15:12 -0700

    updated database info.

--- sixdegrees/Database.jl
+++ sixdegrees/Database.jl
@@ -3,8 +3,8 @@ module Database
 using MySQL
 
 const HOST = "127.0.0.1"
-const USER = "root"
-const PASS = "root"
+const USER = "developer"
+const PASS = "12345678"
 const DB = "six_degrees"
 const PORT = 8889
 
